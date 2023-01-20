2db1ff3056be3ed1c2da755fdc7c467746ea4d23
MichelleNF <a3521110604@alumno.uttehuacan.edu.mx>
2023-01-20 13:14:54 -0600

    Proyecto SPACE v1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface Rocket{
+  name: String;
+  description: String;
+  flickr_images: String;
+  engine: String;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,16 +14,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'space-5B';
-  data: any[] = [];
+  data:  Rocket[] = [];
+  capsule : any [] = [];
+  company : any [] = [];
 
   constructor(
     private http: HttpClient
   ){}
 
   ngOnInit() {
-    this.http.get('https://api.spacexdata.com/v4/rockets').subscribe((data: any) => {
+    this.http.get<Rocket[]>('https://api.spacexdata.com/v4/rockets').subscribe((data: any)  => {
       this.data = data;
     })
+    this.http.get('https://api.spacexdata.com/v4/capsules').subscribe((capsule: any)  => {
+      this.capsule = capsule;
+    })
+    this.http.get('https://api.spacexdata.com/v4/company').subscribe((company: any)  => {
+      this.company = company;
+    })
   }
-
 }
